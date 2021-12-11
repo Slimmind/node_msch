cc598a304bb9a1f7540dd1ac57f48b8729c75649
Paul Siroshtan <slimmind@gmail.com>
2021-12-11 20:47:57 +0200

    initial commit

---
+++ models/cart.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
+
+module.exports = class Cart {
+  constructor() {
+    this.products = [];
+    this.totalPrice = 0;
+  }
+
+  static addProduct(id) {
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if(!err) {
+        cart = JSON.parse(fileContent);
+      }
+    });
+  }
+}
